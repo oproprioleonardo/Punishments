cb79da3ec28c21b50d909ffa3c02e7990411e7c4
TheWanted006 <l.6042silva@gmail.com>
2020-07-01 15:08:53 -0300

    Sistema de punições pronto.

--- src/me/centralworks/punishments/cmds/CmdWarn.java
+++ src/me/centralworks/punishments/cmds/CmdWarn.java
@@ -1,4 +1,15 @@
 package me.centralworks.punishments.cmds;
 
-public class CmdWarn {
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.plugin.Command;
+
+public class CmdWarn extends Command {
+    public CmdWarn(String name, String permission, String... aliases) {
+        super(name, permission, aliases);
+    }
+
+    @Override
+    public void execute(CommandSender commandSender, String[] strings) {
+
+    }
 }
